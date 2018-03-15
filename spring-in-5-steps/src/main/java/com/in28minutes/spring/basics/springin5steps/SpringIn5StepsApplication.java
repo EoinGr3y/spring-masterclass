4beedc2caa9e6a7d1b94989119d79fea8e0c5e61
Eoin Graham <eoin.g08@gmail.com>
2018-03-15 20:40:43 +0000

    Updates from section 1 of spring course, covering components, constructor and setter injection

--- spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsApplication.java
+++ spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsApplication.java
@@ -2,14 +2,15 @@ package com.in28minutes.spring.basics.springin5steps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringIn5StepsApplication {
 
 	public static void main(String[] args) {
-		BinarySearchImpl binarySearch = new BinarySearchImpl(new QuickSortAlgorithm());
-		int result = binarySearch.binarySearch(new int[] { 12, 4, 6 }, 3);
+		ApplicationContext applicationContext = SpringApplication.run(SpringIn5StepsApplication.class, args);
+		BinarySearchImpl binarySearchImpl = applicationContext.getBean(BinarySearchImpl.class);
+		int result = binarySearchImpl.binarySearch(new int[] { 12, 4, 6 }, 3);
 		System.out.println(result);
-//		SpringApplication.run(SpringIn5StepsApplication.class, args);
 	}
 }
