1a002c5fd19a490166ec54ff6111eae0e8af91e1
Eoin Graham <eoin.g08@gmail.com>
2018-04-01 13:25:14 +0100

    Added jdbc queries to create/update/delete

--- database-demo/src/main/java/com/in28minutes/database/databasedemo/DatabaseDemoApplication.java
+++ database-demo/src/main/java/com/in28minutes/database/databasedemo/DatabaseDemoApplication.java
@@ -1,5 +1,7 @@
 package com.in28minutes.database.databasedemo;
 
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,16 +9,17 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.in28minutes.database.databasedemo.entity.Person;
 import com.in28minutes.database.databasedemo.jdbc.PersonJdbcDAO;
 
 @SpringBootApplication
 public class DatabaseDemoApplication implements CommandLineRunner {
 
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
-	
+
 	@Autowired
 	PersonJdbcDAO dao;
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(DatabaseDemoApplication.class, args);
 	}
@@ -24,5 +27,10 @@ public class DatabaseDemoApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		logger.info("All users -> {}", dao.findAll());
+		logger.info("User id 10001 -> {}", dao.findById(10001));
+		logger.info("User location New York -> {}", dao.findByLocation("New York"));
+		logger.info("Deleting 10002 -> {}", dao.deleteById(10002));
+		logger.info("Inserting 10005 -> {}", dao.insert(new Person(10005, "Tara", "Berlin", new Date())));
+		logger.info("Updating 10003 -> {}", dao.update(new Person(10003, "Pieter", "Utrecht", new Date())));
 	}
 }
--- database-demo/src/main/java/com/in28minutes/database/databasedemo/jdbc/PersonJdbcDAO.java
+++ database-demo/src/main/java/com/in28minutes/database/databasedemo/jdbc/PersonJdbcDAO.java
@@ -1,5 +1,6 @@
 package com.in28minutes.database.databasedemo.jdbc;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,13 +12,38 @@ import com.in28minutes.database.databasedemo.entity.Person;
 
 @Repository
 public class PersonJdbcDAO {
-	
+
 	@Autowired
 	JdbcTemplate jdbcTemplate;
-	
+
 	public List<Person> findAll() {
-		return jdbcTemplate.query("select * from Person",
-				new BeanPropertyRowMapper(Person.class));
+		return jdbcTemplate.query("select * from Person", new BeanPropertyRowMapper<Person>(Person.class));
+	}
+
+	public Person findById(int id) {
+		return jdbcTemplate.queryForObject("select * from Person where id=?", new Object[] { id },
+				new BeanPropertyRowMapper<Person>(Person.class));
+	}
+
+	public List<Person> findByLocation(String location) {
+		return jdbcTemplate.query("select * from Person where location=?", new Object[] { location },
+				new BeanPropertyRowMapper<Person>(Person.class));
+	}
+
+	public int deleteById(int id) {
+		return jdbcTemplate.update("delete from Person where id=?", new Object[] { id });
+	}
+
+	public int insert(Person person) {
+		return jdbcTemplate.update("insert into person(id, name, location, birth_date) values(?, ?, ?, ?);",
+				new Object[] { person.getId(), person.getName(), person.getLocation(),
+						new Timestamp(person.getBirthDate().getTime()) });
+	}
+
+	public int update(Person person) {
+		return jdbcTemplate.update("update person set name = ?, location = ?, birth_date = ? "
+				+ "where id = ?", new Object[] {
+				person.getName(), person.getLocation(), new Timestamp(person.getBirthDate().getTime()), person.getId() });
 	}
 
 }
